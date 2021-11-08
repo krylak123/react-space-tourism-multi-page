5f8419e5c54d176950e826a2719270371662d1d3
Krystian Grzelak <krystian.grzelak@o2.pl>
2021-11-08 14:38:30 +0100

    created Home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import classNames from 'classnames';
+
+// eslint-disable-next-line no-unused-vars
+import styles from './Home.module.scss';
+
+const Home = () => {
+  console.log('home component');
+
+  return (
+    <main className={classNames('home')}>
+      <div className={classNames('home__text-wrapper')}>
+        <p className={classNames('home__heading-1')}>so, you want to travel to</p>
+        <h1 className={classNames('home__heading-2')}>space</h1>
+        <p className={classNames('home__heading-3')}>
+          Let’s face it; if you want to go to space, you might as well genuinely go to outer space
+          and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a
+          truly out of this world experience!
+        </p>
+      </div>
+      <button type="button" className={classNames('home__btn')}>
+        explore
+      </button>
+    </main>
+  );
+};
+
+export default Home;
