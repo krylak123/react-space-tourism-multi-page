b17fcb88d48daa36b4a76e9f9ff2a353efc353e5
Krystian Grzelak <krystian.grzelak@o2.pl>
2021-11-10 11:32:57 +0100

    fixed webpack config in production mode

--- webpack/production.js
+++ webpack/production.js
@@ -23,7 +23,7 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: {
-                localIdentName: '[local]-[hash:base64]',
+                localIdentName: '[local]',
               },
             },
           },
